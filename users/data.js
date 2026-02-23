4dc892bcb0090073516a34d604a56ed5cb76a422
orbimatcha <hesapueni200@gmail.com>
2026-02-23 09:42:22 +0300

    Create data.js

---
+++ users/data.js
@@ -0,0 +1,25 @@
+// Auto-generated favorites list
+// Multi-user support - each user has their own favorites
+// Updated: 2026-02-23 06:27:47 UTC
+
+export const favorites = {
+  "816774921": ["love-story-indila", "fnaf-song-1", "sad-song-we-the-kings", "fallen-down", "rush-e", "blue", "its-been-so-long", "pork-soda", "creep", "gravity-falls-main-title-theme"],
+  "451609118": ["love-story-indila"],
+  "36619352": ["erika"],
+  "742785636": ["pork-soda", "all-i-want-for-christmas-is-you"],
+  "43305368": ["all-girls-are-the-same", "stick-season", "pork-soda", "promise", "love-story-indila", "chest-pain-i-love", "if-you-love-these-people", "debito-di-sonno", "five-nights-at-freddys-movie", "his-theme"],
+  "79251703": ["another-love", "izmir-marsi"],
+  "698251037": ["unravel"],
+  "836959497": ["kid-cudi", "his-theme"],
+  "938081548": ["interstellar", "aria-math"],
+  "733578271": ["grandmas-home"],
+  "156008784": ["song-of-storms", "grandmas-home", "eternal-dream-murder-drones", "creep", "tough-luck", "new-computers"],
+  "198019641": ["robbery-juice-wrld"],
+  "112398055": ["chest-pain-i-love"],
+  "639401484": ["super-mario-bros-theme", "orcalero-orcala-funk", "moonlight-on-the-river", "old-doll", "dearly-beloved", "back-to-friends", "love-wave-to-earth", "strangers", "touch-tone-telephone"],
+  "457769316": ["five-nights-at-freddys-movie", "i-wanna-be-yours"],
+  "891179348": [],
+  "858946055": ["pork-soda"],
+  "848879732": ["carol-of-the-bells", "from-the-start", "the-world-death-note-op", "if-i-am-with-you", "never-meant-to-belong", "the-cruel-angels-thesis", "kawaikute-gomen", "nope-your-too-late-i-already-died", "poster-boy"],
+  "943232059": ["promise"]
+};
